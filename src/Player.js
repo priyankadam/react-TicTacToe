856f5ce2a33e91b50ec12187d69c278a8067d760
priyankadam <kadampriyanka964@gmail.com>
2020-05-27 23:16:46 +0530

    game complete

---
+++ src/Player.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class Player extends Component{
+	
+
+handleForm(e){
+	e.preventDefault();
+  this.props.player(e.target.player.value);
+}
+ render()
+
+
+{
+
+	return(
+   < form onSubmit={(e)=> this.handleForm(e)}>
+   <lable>
+
+   Player X
+   <input type="radio" name="player" value="X"/>
+ 
+    </lable>
+
+   <lable>
+
+   Player O
+   <input type="radio" name="player" value="O"/>
+ 
+    </lable>
+    <input type="submit" value="start"/>
+   </form>
+   )
+}
+
+}
+
+export default Player;
